9752c326857e691d922469895b5e913c1f342470
sergioag <sergio.alfaro@serbatic.es>
2025-11-13 13:28:37 +0100

    first commit

---
+++ src/main/java/com/sergioag/clinicare_api/ClinicareApiApplication.java
@@ -0,0 +1,14 @@
+package com.sergioag.clinicare_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicareApiApplication.class, args);
+        System.out.println("Holi");
+	}
+
+}
---
+++ src/test/java/com/sergioag/clinicare_api/ClinicareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergioag.clinicare_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
